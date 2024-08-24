eadead0289c662c8989a68434b12d918a34915f5
Greg McCarthy <gregmccarthy@Gregs-MacBook-Pro.local>
2024-08-24 16:23:46 +0100

    added logging

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+import logging
+import logging.config
+
+
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
+
 @app.get("/hello")
 def hello():
+    logger.error('test error')
     return {"Hello": "How are you doing?"}
 
