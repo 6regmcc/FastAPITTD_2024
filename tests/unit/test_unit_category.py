9cf94eb315984436dda3841621af1948bc2e181f
Greg McCarthy <gregmccarthy@Gregs-MacBook-Pro.local>
2024-09-10 11:52:13 +0100

    added unit create new category

--- tests/unit/test_unit_category.py
+++ tests/unit/test_unit_category.py
@@ -1,11 +1,15 @@
 import pytest
+from fastapi import HTTPException
 from pydantic import ValidationError
 
 from app.schemas.category_schema import CategoryCreate
 from tests.factories.modles_factory import get_random_category_dict
+from app.models import Category
+
 
 def mock_output(return_value=None):
     return lambda *args, **kwargs:return_value
+
 
 def test_unit_schema_category_validation():
     valid_data = {"name": "test_category", "slug": "test_slug"}
@@ -25,11 +29,11 @@ def test_unit_schema_category_validation():
         CategoryCreate(**invalid_data)
 
 
-
-
-
 def test_unit_create_new_category_successfully(client, monkeypatch):
     category = get_random_category_dict()
+
+    for key, value in category.items():
+        monkeypatch.setattr(Category, key, value)
 
     monkeypatch.setattr("sqlalchemy.orm.Query.first", mock_output())
     monkeypatch.setattr("sqlalchemy.orm.Session.commit", mock_output())
@@ -41,3 +45,25 @@ def test_unit_create_new_category_successfully(client, monkeypatch):
     response = client.post("api/category", json=body)
     assert response.status_code == 201
     assert response.json() == category
+
+
+@pytest.mark.parametrize("existing_category, category_data, expected_detail", [
+
+    (True, get_random_category_dict(), "category with this name and level exists"),
+    (True, get_random_category_dict(), "category with this slug and level exists")
+])
+def test_unit_create_new_category_existing(client, monkeypatch, existing_category, category_data, expected_detail):
+    def mock_check_existing_category(db, category_data):
+        if existing_category:
+            raise HTTPException(status_code=400, detail=expected_detail)
+
+    monkeypatch.setattr("app.routers.category_routes.check_existing_category", mock_check_existing_category)
+
+    monkeypatch.setattr("sqlalchemy.orm.Query.first", mock_output())
+    body = category_data.copy()
+    body.pop("id")
+    response = client.post("api/category/", json=body)
+    assert response.status_code == 400
+
+    if expected_detail:
+        assert response.json() == {"detail": expected_detail}
